bbb59d008345a93827718414399c82be66146a71
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-04-06 11:05:09 +0300

    2.5.kurssitiedot

---
+++ Kurssitiedot/src/components/Course.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+
+
+const Course = ({course}) => {
+    
+  return(
+    <div>
+      <Header course = {course} />
+      <Content course={course} />
+      <Total course = {course} />
+    </div>
+    
+  )
+
+}
+
+const Header = ({course})=> {
+  return (
+    <div>
+      <h1>
+        {course.name}
+      </h1>
+    </div>
+  )
+}
+
+const Total = ({course})=> {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const tehtavat = (course.parts.map(part  => part.exercises));
+
+  return (
+    <div>
+      <p>
+        yhteensä {tehtavat.reduce(reducer,0)} tehtävää
+      </p>
+    </div>
+  )
+} 
+
+const Part = ({name, exercises})=> {
+  return (
+      <li> 
+          {name} {exercises}
+      </li> 
+  )
+}
+
+const Content = ({course})=> {
+  const rows = () => course.parts.map(part =>
+      <Part
+        key={part.id}
+        name={part.name}
+        exercises = {part.exercises}
+      />
+  )   
+  return (
+    <ul>
+        {rows()}
+    </ul>
+  )
+}
+
+export default Course
+
+
+
