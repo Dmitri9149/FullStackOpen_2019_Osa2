fc7a24ea77cb3b1045a7830b80f5054879deb39a
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-04-10 19:00:30 +0300

    2.12.maiden_tiedot

--- maiden_tiedot/src/components/Country.js
+++ maiden_tiedot/src/components/Country.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 const Country = ({ country }) => {
   return (
-    <li>
+    <div>
       <h2>{country.name} {country.altSpellings[1]}</h2>
       <p>capital: {country.capital}</p>
       <p>population:{country.population}</p>
+      <ul>
+       {country.languages.map(lang =><li key = {lang.name}> {lang.name}</li>)} 
+      </ul>
       <img max-width = {250} height = {80} src={country.flag} alt="Flag" />
       
-    </li>
+    </div>
   )
 }
 
