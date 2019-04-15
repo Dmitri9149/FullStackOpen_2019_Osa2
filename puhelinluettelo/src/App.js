d37714359e58fbbf72a9f99a232e5ea7a5bd92ca
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-04-15 15:49:15 +0300

    axios is changed to personService

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from "./components/PersonForm"
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [ persons, setPersons] = useState([]) 
@@ -12,14 +13,14 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios
+    personService
       .get('http://localhost:3001/persons')
       .then(response => {
         console.log('promise fulfilled')
         setPersons(response.data)
       })
   }, [])
-  
+
   console.log('render', persons.length, 'persons') 
   
 
@@ -34,7 +35,7 @@ const App = () => {
         number:newNumber 
       }
 
-      axios
+      personService
       .post('http://localhost:3001/persons', personObject)
       .then(response => {
         setPersons(persons.concat(response.data))
